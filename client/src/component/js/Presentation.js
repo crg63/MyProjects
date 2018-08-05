25494c94169662989607d3a2bcdf5539ad8a41c2
admin <floriancargouet@gmail.com>
2018-08-05 15:46:00 +0200

    - try fetch

--- client/src/component/js/Presentation.js
+++ client/src/component/js/Presentation.js
@@ -4,31 +4,6 @@ import '../css/Presentation.css'
 import '../css/NextPage.css'
 
 class Presentation extends React.Component {
-		 state = {
-		    response: ''
-		  };
-
-	componentDidMount() {
-		 // this.callApi()
-	      // .then(res => this.setState({ response: res.express }))
-	      // .catch(err => console.log(err));
-  				fetch("/data/projects")
-				.then(res=>res.json())
-				.then((data)=> {
-					console.log(data);
-					this.setState({response: data.express})
-				})
-	}
-
-	// callApi = async () => {
- //    const response = await fetch('/api/hello');
- //    const body = await response.json();
-
- //    if (response.status !== 200) throw Error(body.message);
-
- //    	return body;
- //  	}
-
 	render() {
 		return (
 			<div className="Presentation" >
