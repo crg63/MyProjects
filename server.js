99f83666982d9b41a1ca06727ec18cef0ff6f52f
crg63 <floriancargouet@gmail.com>
2018-08-07 15:42:44 +0200

    try to use body-parser and mongodb

--- server.js
+++ server.js
@@ -1,24 +1,127 @@
 const express = require('express');
 const fs = require('fs');
-// import createDB from "./demo_create_mongo_db.js"
-
 const MongoClient = require('mongodb').MongoClient;
-var mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node-demo");
-
-
-// console.log(MongoClient)
+ 
+//L'application requiert l'utilisation du module Express.
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.  
+// var express = require('express');
+ 
+// Nous définissons ici les paramètres du serveur.
+// var hostname = 'localhost'; 
+ 
+// La variable mongoose nous permettra d'utiliser les fonctionnalités du module mongoose.
+var mongoose = require('mongoose'); 
+// Ces options sont recommandées par mLab pour une connexion à la base
+// var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+// replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+ 
+//URL de notre base
+const urlmongo = "mongodb://florian:fsimpson78@ds139219.mlab.com:39219/projects"
+ 
+// Nous connectons l'API à notre base de données
+// mongoose.connect(urlmongo, options);
+ 
+// var db = mongoose.connection; 
+// db.on('error', console.error.bind(console, 'Erreur lors de la connexion')); 
+// db.once('open', function (){
+//     console.log("Connexion à la base OK"); 
+// }); 
 
 const app = express();
+
+var bodyParser = require("body-parser"); 
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+
+// MongoClient.connect(urlmongo, (err, db) => {
+// 	if (err) {
+// 		return console.log("Fail",err)
+// 	}
+// 	if (db) {
+// 				return console.log(db)
+
+// 	}
+
+// 	db.collection('notes').insertOne(
+// 	{
+// 		title: 'Hello MongoDB',
+// 		text: 'Hopefully this works!'
+// 	},
+// 	function (err, res) {
+// 			if (err) {
+// 				db.close();
+// 				return console.log(err);
+// 			}
+// 			if (res) {
+// 				return console.log("CONNECTED")
+// 			}
+// 			// Success
+// 			db.close();
+// 		}
+// 	)
+// });
+
+var projectSchema =  mongoose.Schema({
+	title: String,
+	pour: String,
+	avec: String,
+	description: String,
+	img: { data: Buffer, contentType: String }
+
+})
+
+var contactSchema =  mongoose.Schema({
+	LastName: String,
+	FirstName: String,
+	Email: String,
+	Subject: String
+})
+
+var db
+
+
+MongoClient.connect(urlmongo, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('projects') // whatever your database name is
+  app.listen(3001, () => {
+    console.log('listening on 3001')
+  })  
+})
+
+app.post('/projects', (req, res) => {
+  db.collection('projects').save(req.body, (err, result) => {
+    console.log(req.body)
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/#/Contact')
+  })
+})
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('projects').find()
+})
+
+// db.collection('projects').find().toArray(function(err, results) {
+//   console.log(results)
+//   // send HTML file populated with quotes here
+// })
+
+// var Project = mongoose.model('Project', contactSchema)
+
+
+
+
+
+
 const port = process.env.PORT || 5000;
 
 // let rawdata = fs.readFileSync('projects.json');  
 var projects = JSON.parse(fs.readFileSync('projects.json'));  
 
 app.get('/data/projects', (req, res) => {
-	res.send({  projects});
+	res.send({projects});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
