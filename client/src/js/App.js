1b59dc367528a79c49d5d557aef42ec6a5537687
admin <floriancargouet@gmail.com>
2018-11-03 13:22:41 +0100

    ~ path changed

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Main from './Main.js'
-import '../css/App.css'
+import '../App.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faPaperPlane, faStroopwafel } from '@fortawesome/free-solid-svg-icons'
 
