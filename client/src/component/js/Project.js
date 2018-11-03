4418248ff65669ed11d372b52b90caf8a0a68776
admin <floriancargouet@gmail.com>
2018-11-03 13:49:34 +0100

    + popup

--- client/src/component/js/Project.js
+++ client/src/component/js/Project.js
@@ -1,69 +1,12 @@
 import React from 'react'
+import SkyLight from 'react-skylight'
+import Clap from './Clap.js'
 
 var nbOfComponentMounted = -1
 
 class BtnClap extends React.Component {
-	// constructor(props) {
-	// 		super(props)
-	// 		this.state = {applose: this.props.applose}
-	// 		this.MoreApplose = this.MoreApplose.bind(this);
-	// 	}
 
-	// componentDidMount() {
-	// 	fetch("https://api.mlab.com/api/1/databases/projects/collections/projects?f={%22apploseNb%22:%201}&apiKey=fF9iSxFIZA8zMgnrw9eNdbLGakDxQx1V", {method: 'GET'})
-	// 	.then(res => res.json())
-	// 	.then(data => {
-	// 		nbOfComponentMounted += 1
-	// 		if (nbOfComponentMounted === data.length) {
-	// 			nbOfComponentMounted = 0
-	// 		}
-	// 		this.setState({ applose: 0 })
-	//   });
-	// 	}
-
-	// MoreApplose() {
-	// 	// var newApplose = this.state.applose + 1
-	// 	// this.setState({applose: newApplose})
-	// 	fetch('/clicked', {method: 'POST'})
-	// 	.then(function(response) {
-	// 		if(response.ok) {
-	// 			console.log('click was recorded');
-	// 			return;
-	// 		}
-	// 		throw new Error('Request failed.');
-	// 	})
-	// 	.catch(function(error) {
-	// 		console.log(error);
-	// 	});
-
-	// 	setInterval(function() {
-	// 		fetch('/clicks', {method: 'GET'})
-	// 		.then(function(response) {
-	// 			if(response.ok) return response.json();
-	// 			throw new Error('Request failed.');
-	// 		})
-	// 		.then(function(data) {
-	// 			document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
-	// 		})
-	// 		.catch(function(error) {
-	// 			console.log(error);
-	// 		});
-	// 	}, 1000);
-	// }
-
-		render() {
-		return (
-		  <div className="btnClap">
-		  	<p> {this.state.applose} </p>
-		    <button onClick={this.MoreApplose}>
-				<img src={require("../../img/y.png")} /> 
-			</button>
-		  </div>
-		);
-	}
 }
-
-			
 
 class Project extends React.Component {
 	componentDidMount() {
@@ -76,7 +19,10 @@ class Project extends React.Component {
 			<div className="Project" id={this.props.data.name}>
 				<div className="imgProject">
 					<a href={this.props.data.link} target="_blank"> <img src={this.props.data.img} alt="Web site" /> </a>
-					{/*<button type="submit" value="Send" className="btnInfo" >+ Plus d'info </button>*/}
+					<button onClick={() => this.simpleDialog.show()} className="btnInfo" >+ Plus d'info </button>
+					<SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title={this.props.data.name}>
+			         {this.props.data.description}
+			        </SkyLight>
 					{/*<p id="counter"> </p> */}
 				</div>
 				<div className="infoProject">
@@ -85,6 +31,7 @@ class Project extends React.Component {
 						<p>Réalisé avec: {this.props.data.avec}</p>
 						<p></p>
 						{/*<BtnClap onClick={this.MoreApplose} applose={this.props.data.apploseNb} />	*/}
+						{/*<Clap onClick={this.MoreApplose} applose={this.props.data.apploseNb} />*/}
 					</div>
 				</div>
 			</div>
