af664870ee9bf27bac4b66b6f95113e5d74d665f
crg63 <floriancargouet@gmail.com>
2018-12-20 00:00:06 +1000

    +footer

---
+++ client/src/component/js/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { HashLink as Link } from 'react-router-hash-link';
+import '../css/Footer.css'
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer className="Footer">
+					<ul>
+						<li>
+							<a href="http://site-web-france.com/Presentation"> Présentation </a>
+						</li>
+						<li>
+							<a href="http://site-web-france.com/Projects"> Mes réalisations </a>
+						</li>
+						<li>
+							<a href="http://site-web-france.com/contact"> Contact </a>
+						</li>
+					</ul>
+					<ul>
+						<li> © 2018 - Florian Cargouet  </li>|
+						<li> <a href="">Mention légale </a> </li>
+					</ul>
+			</footer>
+		)
+	}
+}
+
+export default Footer
